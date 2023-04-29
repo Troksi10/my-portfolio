62cb7cc7e4ab3e45809002bc7e8798714637384b
lLija <ilija.trokovski@gmail.com>
2023-04-29 23:28:24 +0200

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./FooterStyle.css";
+import {
+  FaFacebook,
+  FaHome,
+  FaInstagram,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>ul.Tashko Karadja br.3/1-7</p>
+              <p>North Macedonia</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              +38978428085
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              ilija.webprogrammer@gmail.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>We All Wear Masks</h4>
+          <p>First, solve the problem. Then, write the code.</p>
+          <p>
+            When I wrote this code, only God and I understood what I did. Now
+            only God knows.
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaInstagram
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
